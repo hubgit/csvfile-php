d1722ccff09eb0ac74418a23de7e7dcf4d2a6905
Alf Eaton <alf@hubmed.org>
2014-02-12 13:26:07 +0000

    simplify example

--- demo/demo.php
+++ demo/demo.php
@@ -15,17 +15,3 @@ $csvfile->read(function($item) use (&$items) {
 });
 
 print_r($items);
-
-// header outside file
-
-$description->header = ["id", "title", "volume", "date published"];
-
-$csvfile = new CSVFile(__DIR__ . '/no-header.csv', $description);
-
-$items = array();
-
-$csvfile->read(function($item) use (&$items) {
-	$items[$item['id']] = $item;
-});
-
-print_r($items);
